6bb7ccd12ab54c5fe01008947c260ade02e14c0a
HedvigM <hedvig@mejstedt.se>
2021-11-22 09:58:42 +0100

    card component + prettierrc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components/macro';
+import { Card } from 'lib/card';
+
+const ThinnerCard = styled(Card)`
+  width: 400px;
+`;
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <>
+      <ThinnerCard
+        coverImage="https://picsum.photos/500/300"
+        thumbnailUrl="https://picsum.photos/100/100"
+        title="This is the Title"
+        secondaryText="Secondary Title"
+      />
+      <Card title="Title" secondaryText="Secondary text" />
+    </>
+  );
+};
